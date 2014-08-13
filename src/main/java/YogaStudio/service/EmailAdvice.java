9daad3274ec563e5deaeb553f8dd919b7ac5132c
demodem <demodem@CS_Lab115-106.cs.mum.edu>
2014-08-12 19:30:29 -0500

    true	EmailAdvice.java	-/Modified	Commit	src\main\java\YogaStudio\service\EmailAdvice.java true	rootconfig.xml	-/Modified	Commit	src\main\webapp\WEB-INF\rootconfig.xml

--- src/main/java/YogaStudio/service/EmailAdvice.java
+++ src/main/java/YogaStudio/service/EmailAdvice.java
@@ -5,13 +5,14 @@
  */
 package YogaStudio.service;
 
+
 import YogaStudio.domain.FacultyEntity;
 import YogaStudio.domain.UserEntity;
+import YogaStudio.domain.WaiverEntity;
 import java.util.Date;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -20,70 +21,99 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 @Aspect
 public class EmailAdvice {
+
     @Autowired
     private EmailService emailservice;
     @Autowired
     private UserService userservice;
+    @Autowired
+    private WaiverService waiverService;
 
-    public EmailAdvice(EmailService emailservice,UserService userservice) {
+    public EmailAdvice(EmailService emailservice, UserService userservice, WaiverService waiverService) {
         this.emailservice = emailservice;
         this.userservice = userservice;
+        this.waiverService = waiverService;
     }
-    
+
     @After("execution(* YogaStudio.service.UserService.add(..))&& args( fullname, email,username, dateOfBirth, street,city,country, state, zipcode,contactNum,authority)")
-    public void sendEmail(JoinPoint joinpoint, String fullname, 
-                       String email,
-                       String username, 
-                       Date dateOfBirth,
-                       String street,
-                       String city,
-                       String country,
-                       String state,
-                       Long zipcode,
-                       Long contactNum,
-                       String authority) {
-        try{
-            UserEntity user = userservice.findUserBy("username",username);
+    public void sendEmail(JoinPoint joinpoint, String fullname,
+            String email,
+            String username,
+            Date dateOfBirth,
+            String street,
+            String city,
+            String country,
+            String state,
+            Long zipcode,
+            Long contactNum,
+            String authority) {
+        try {
+            UserEntity user = userservice.findUserBy("username", username);
             String subject = "Welcome YogaStudio.";
-            String message = "Dear " + user.getFullname() + ",\r\n\r\n" + "Welcome to YogaStudio.\r\n\r\n" + "Your password is: " + user.getPassword() + 
-                    ". \r\n\r\n" + "Regards\r\n\r\nYogaStudio INC.\r\n\r\n"
-                    ;
-                    
-            emailservice.sendEmail(email,subject,message);
-        }
-        catch(Exception e){
-         System.out.println("Error sending email "+e.getMessage());
+            String message = "Dear " + user.getFullname() + ",\r\n\r\n" + "Welcome to YogaStudio.\r\n\r\n" + "Your password is: " + user.getPassword()
+                    + ". \r\n\r\n" + "Regards\r\n\r\nYogaStudio INC.\r\n\r\n";
+
+            emailservice.sendEmail(email, subject, message);
+        } catch (Exception e) {
+            System.out.println("Error sending email " + e.getMessage());
         }
 
     }
-    
+
     @After("execution(* YogaStudio.service.FacultyService.add(..))&& args( facultyEntity)")
     public void sendEmailFaculty(JoinPoint joinpoint, FacultyEntity facultyEntity) {
-        try{
+        try {
             String subject = "Welcome YogaStudio.";
-            String message = "Dear " + facultyEntity.getFullname() + ",\r\n\r\n" + "Welcome to YogaStudio.\r\n\r\n" + "You added as a faculty.\r\n\r\nYour user name is: "+ facultyEntity.getUsername()+ " \r\n\r\nYour password is: " + facultyEntity.getPassword() + 
-                    ". \r\n\r\n" + "Regards\r\n\r\nYogaStudio INC.\r\n\r\n"
-                    ;
-                    
-            emailservice.sendEmail(facultyEntity.getEmail(),subject,message);
-        }
-        catch(Exception e){
-         System.out.println("Error sending email "+e.getMessage());
+            String message = "Dear " + facultyEntity.getFullname() + ",\r\n\r\n" + "Welcome to YogaStudio.\r\n\r\n" + "You added as a faculty.\r\n\r\nYour user name is: " + facultyEntity.getUsername() + " \r\n\r\nYour password is: " + facultyEntity.getPassword()
+                    + ". \r\n\r\n" + "Regards\r\n\r\nYogaStudio INC.\r\n\r\n";
+
+            emailservice.sendEmail(facultyEntity.getEmail(), subject, message);
+        } catch (Exception e) {
+            System.out.println("Error sending email " + e.getMessage());
         }
 
     }
-    
-     @After("execution(* YogaStudio.service.UserService.add(..))&& args( id)")
-    public void sendEmail1(JoinPoint joinpoint, Long id) {
-        try{
-           
-            //System.out.println("khanuser");        
-           // emailservice.generateEmailForNewAppRegistration(email);
-        }
-        catch(Exception e){
-         System.out.println("Error sending email "+e.getMessage());
+
+    @After("execution(* YogaStudio.service.WaiverService.rejectWaiverRequest(..))&& args(waiverID,comments)")
+    public void sendRejectedEmail(Long waiverID, String comments) {
+        try {
+            WaiverEntity waiver = waiverService.getWaiver(waiverID);
+            String subject = "Update on Waiver Request.";
+            String message = "Dear " + waiver.getCustomer().getFullname() + ",\r\n\r\n" + "Your waiver request is Rejected by FA.\r\n\r\n" + "Waiver Details: " + "\n Waiver ID:" + waiver.getId() + "\n Class Name:" + waiver.getYogaClass().getClassName() + "\n Faculty Advisor:" + waiver.getFaculty().getFullname() + "\n Status:" + waiver.getStatus() + "\n Comments:" + waiver.getComments() + "\n Please login for full details."
+                    + ". \r\n\r\n" + "Regards\r\n\r\nYogaStudio INC.\r\n\r\n";
+
+            emailservice.sendEmail(waiver.getCustomer().getEmail(), subject, message);
+        } catch (Exception e) {
+            System.out.println("Error sending email " + e.getMessage());
         }
 
-    } 
+    }
+
+    @After("execution(* YogaStudio.service.WaiverService.approveWaiverRequest(..))&& args(waiverID,comments)")
+    public void sendApprovedEmail(Long waiverID, String comments) {
+        try {
+            WaiverEntity waiver = waiverService.getWaiver(waiverID);
+            String subject = "Update on Waiver Request.";
+            String message = "Dear " + waiver.getCustomer().getFullname() + ",\r\n\r\n" + "Your waiver request is Accepted by FA. You can proceed with enrollment.\r\n\r\n" + "Waiver Details: " + "\n Waiver ID:" + waiver.getId() + "\n Class Name:" + waiver.getYogaClass().getClassName() + "\n Faculty Advisor:" + waiver.getFaculty().getFullname() + "\n Status:" + waiver.getStatus() + "\n Comments:" + waiver.getComments() + "\n Please login for full details."
+                    + ". \r\n\r\n" + "Regards\r\n\r\nYogaStudio INC.\r\n\r\n";
+
+            emailservice.sendEmail(waiver.getCustomer().getEmail(), subject, message);
+        } catch (Exception e) {
+            System.out.println("Error sending email " + e.getMessage());
+        }
+
+    }
+
+    @After("execution(* YogaStudio.service.UserService.add(..))&& args( id)")
+    public void sendEmail1(JoinPoint joinpoint, Long id) {
+        try {
+
+            //System.out.println("khanuser");        
+            // emailservice.generateEmailForNewAppRegistration(email);
+        } catch (Exception e) {
+            System.out.println("Error sending email " + e.getMessage());
+        }
+
+    }
 
 }
